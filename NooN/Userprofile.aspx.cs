c23cdeed6dcbfc2226533821a29d62836db6b31e
yaseen khattab <khattabyasen@gmail.com>
2026-07-10 18:38:25 +0300

    auto: update Userprofile.aspx.cs

--- NooN/Userprofile.aspx.cs
+++ NooN/Userprofile.aspx.cs
@@ -6,7 +6,7 @@ namespace NooN
 {
     public partial class Userprofile : System.Web.UI.Page
     {
-        string connStr = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+        string connStr = Db.ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
