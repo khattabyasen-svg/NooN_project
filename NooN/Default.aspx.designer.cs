bf471243e7a1f532a14625f4d4c1a1b897ad2c50
yaseen khattab <khattabyasen@gmail.com>
2026-07-10 19:07:30 +0300

    auto: update Default.aspx.designer.cs

--- NooN/Default.aspx.designer.cs
+++ NooN/Default.aspx.designer.cs
@@ -87,6 +87,15 @@ namespace NooN
         protected global::System.Web.UI.WebControls.Panel pnlNoProducts;
 
         /// <summary>
+        /// upProducts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.UpdatePanel upProducts;
+
+        /// <summary>
         /// rptProducts control.
         /// </summary>
         /// <remarks>
